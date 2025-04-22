b27918a816f51f3515dc277a892579655485e0ed
sug4chy <sb.9226981433@gmail.com>
2025-04-22 18:38:32 +0500

    Добавил третьего пользователя на Staging

---
+++ UniCast.Infrastructure.Persistence/Migrations/20250422090519_Staging__AddUser3.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Hosting;
+
+#nullable disable
+
+namespace UniCast.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Staging__AddUser3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Staging)
+            {
+                migrationBuilder.Sql(
+                    """
+                    INSERT INTO student(id, full_name, group_id)
+                    VALUES (gen_random_uuid(), 'Антонов Антон', (SELECT id FROM academic_group))
+                    ON CONFLICT DO NOTHING;
+
+                    INSERT INTO moodle_account(id, ext_id, username, student_id) 
+                    VALUES (gen_random_uuid(), 7, 'user3', (SELECT id FROM student WHERE full_name = 'Антонов Антон'))
+                    ON CONFLICT DO NOTHING;
+                    """
+                );
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
